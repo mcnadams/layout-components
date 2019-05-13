28d0ff029ac0ec6e87619d4bec9f91f42effeafd
mcnadams <mcneilbo@oregonstate.edu>
2019-05-13 13:41:47 -0700

    add a dog

--- src/components/app.js
+++ src/components/app.js
@@ -1,5 +1,22 @@
 import React from 'react';
 
 export default function App() {
-  return <h1>Hi!</h1>;
+  const dog = {
+    name: 'Spot',
+    age: 5,
+    weight: '20 lbs'
+  };
+
+  return (
+    <dl>
+      <dt>Name</dt>
+      <dd>{dog.name}</dd>
+
+      <dt>Age</dt>
+      <dd>{dog.age}</dd>
+
+      <dt>Weight</dt>
+      <dd>{dog.weight}</dd>
+    </dl>
+  );
 }
