9c8749016a5ecacd1cc173abde3b9cacfcd23a64
mcnadams <mcneilbo@oregonstate.edu>
2019-05-13 13:39:09 -0700

    start using React

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # layout-components
 webpack SPA stuff
 Webpack is a node program that will be used to create our front end
+
+JSX is HTML looking JavaScript that can be used to create React elements. Anywhere you use JSX you must import react
--- src/index.js
+++ src/index.js
@@ -1,16 +1,9 @@
-//import { unlink } from "fs";
-import styles from './index.css';
+import React from 'react';
+import { render } from 'react-dom';
+import App from './components/app';
 
-const root = document.getElementById('root');
-const list = document.createElement('ul');
-list.className = styles.dogs;
 
-root.appendChild(list);
-
-const dogs = ['rover', 'bingo', 'fido', 'banjo'];
-
-dogs.forEach(dog => {
-  const li = document.createElement('li');
-  li.textContent = dog;
-  list.appendChild(li);
-});
+render(
+  <App />,
+  document.getElementById('root')
+);
