6f06c7d37ac1fd4e74331250ad8a5314503bfdd2
mcnadams <mcneilbo@oregonstate.edu>
2019-05-13 11:55:21 -0700

    some pretty cool stuff

--- src/index.js
+++ src/index.js
@@ -1,2 +1,16 @@
-// eslint-disable-next-line no-console
-console.log('hihihi');
+//import { unlink } from "fs";
+import styles from './index.css';
+
+const root = document.getElementById('root');
+const list = document.createElement('ul');
+list.className = styles.dogs;
+
+root.appendChild(list);
+
+const dogs = ['rover', 'bingo', 'fido', 'banjo'];
+
+dogs.forEach(dog => {
+  const li = document.createElement('li');
+  li.textContent = dog;
+  list.appendChild(li);
+});
